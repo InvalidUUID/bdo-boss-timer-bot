e0ccc085000682a350c85aadc994a882bf51079d
Tobiah <tobiah@warframe.gg>
2020-04-11 15:24:18 -0500

    for syntax

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ async def print_next_boss_message(boss_name, boss_time, channel, is_today):
     # embed = discord.Embed(description=join_boss_descriptions(boss_name), timestamp=when)
     embed.set_footer(text='Spawns', icon_url='https://i.imgur.com/6qzL6l4.png')
     embed.set_author(name=" & ".join(join_bosses(boss_name)), icon_url=boss_name[0]['avatar'])
-    for(boss in boss_name):
+    for boss in boss_name:
         embed.add_field(boss['name'], boss_descrip(boss), True)
     await channel.send(embed=embed)
 
