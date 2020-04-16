ec166a68624933fe4255c3552e879fdee289b0cb
Joey Vienneau <joey.vienneau@gmail.com>
2020-04-15 22:45:40 -0300

    Revert

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ async def print_boss_message(boss_name, channel, delta):
 
 def boss_descrip(boss):
     '''Build the boss description'''
-    return str('Spawns at {location}'.format(location=boss['location']))
+    return str('@{location}'.format(location=boss['location']))
 
 
 async def print_next_boss_message(boss_name, boss_time, channel, is_today):
@@ -36,7 +36,7 @@ async def print_next_boss_message(boss_name, boss_time, channel, is_today):
     # force boss_time onto when
     when = when.replace(hour=boss_time_tokens[0], minute=boss_time_tokens[1])
 
-    embed = discord.Embed(timestamp=when)
+    embed = discord.Embed(timestamp=when, color=boss_name[0]['color'])
     embed.set_footer(text='Spawns', icon_url='https://i.imgur.com/6qzL6l4.png')
     embed.set_thumbnail(url=boss_name[0]['avatar'])
 
@@ -44,7 +44,7 @@ async def print_next_boss_message(boss_name, boss_time, channel, is_today):
 
     # Add all the boss information, first names & spawn locations
     for boss in boss_name:
-        field_num += 1
+        # field_num += 1
         embed.add_field(name=boss['name'], value=boss_descrip(boss), inline=True)
         embed.add_field(name='Recommendations', value=boss['recommendations']
                         + '\n\n:link: [More Info]({link})'.format(
