28f862bd5259ff91384b01dce301d25631b8c694
Joey Vienneau <joey.vienneau@gmail.com>
2020-04-16 00:24:01 -0300

    Strings changed.

--- bot.py
+++ bot.py
@@ -108,7 +108,7 @@ async def addme(ctx, *, boss_name):
         await user.add_roles(role)
         await ctx.send('You will be notified when **{boss_name}** spawns :)'.format(boss_name=boss_name))
     else:
-        ctx.send('I could not find a boss name in your message. Please check that you\'ve spelled the boss name correctly and try again')
+        ctx.send('I could not find a boss name in your message. Please check that you\'ve spelled the boss name correctly and try again.')
     # user = ctx.message.author
     # role = discord.utils.get(ctx.guild.roles, name='Boss Timer')
     # await user.add_roles(role)
@@ -124,7 +124,7 @@ async def removeme(ctx, *, boss_name):
         await user.remove_roles(role)
         await ctx.send('You will no longer be notified when **{boss_name}** spawns :('.format(boss_name=boss_name))
     else:
-        ctx.send('I could not find a boss name in your message. Please check that you\'ve spelled the boss name correctly and try again')
+        ctx.send('I could not find a boss name in your message. Please check that you\'ve spelled the boss name correctly and try again.')
     # role = discord.utils.get(ctx.guild.roles, name='Boss Timer')
     # await user.remove_roles(role)
     # await ctx.send('You will no longer be notified when the next boss spawns :(')
