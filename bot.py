04d78127f63233bbe7fe781bbf7fde944e71b51b
Joey Vienneau <joey.vienneau@gmail.com>
2020-04-16 00:38:50 -0300

    Check if role is none

--- bot.py
+++ bot.py
@@ -105,8 +105,11 @@ async def addme(ctx, *, boss_name):
     user = ctx.message.author
     try:
         role = discord.utils.get(ctx.guild.roles, name=boss_name)
-        await user.add_roles(role)
-        await ctx.send('You will be notified when **{boss_name}** spawns :)'.format(boss_name=boss_name))
+        if role is not None:
+            await user.add_roles(role)
+            await ctx.send('You will be notified when **{boss_name}** spawns :)'.format(boss_name=boss_name))
+        else:
+            ctx.send('Please check that you\'ve spelled the boss name correctly and try again.')
     except KeyError:
         ctx.send('Please check that you\'ve spelled the boss name correctly and try again.')
     # user = ctx.message.author
@@ -121,8 +124,11 @@ async def removeme(ctx, *, boss_name):
     user = ctx.message.author
     try:
         role = discord.utils.get(ctx.guild.roles, name=boss_name)
-        await user.remove_roles(role)
-        await ctx.send('You will no longer be notified when **{boss_name}** spawns :('.format(boss_name=boss_name))
+        if role is not None:
+            await user.remove_roles(role)
+            await ctx.send('You will no longer be notified when **{boss_name}** spawns :('.format(boss_name=boss_name))
+        else:
+            ctx.send('Please check that you\'ve spelled the boss name correctly and try again.')
     except KeyError:
         ctx.send('Please check that you\'ve spelled the boss name correctly and try again.')
     # role = discord.utils.get(ctx.guild.roles, name='Boss Timer')
